e106c83a6a653a71185730bd50776b09c808f1c8
Jurre Groenendijk <jurre@jilles.com>
2020-08-20 21:36:36 +0200

    Implement stack and call/return

--- computer.py
+++ computer.py
@@ -5,13 +5,21 @@ R3 = 2
 R4 = 3
 ACC = 4
 IP = 5
-REGS = ['R1','R2','R3','R4','ACC','IP']
+SP = 6
+FP = 7
+REGS = ['R1','R2','R3','R4','ACC','IP', 'SP', 'FP']
 
 
-MOV_CONST_REG = 0x11   # Move constant into register
-MOV_REG_REG = 0x12     # Move register into register
-ADD_REG_REG = 0x20     # Add values from registers together into ACC
-PRINT_REGISTERS = 0x30 # Print values of all registers
+MOV_LIT_REG   = 0x11 # Move constant into register
+MOV_REG_REG     = 0x12 # Move register into register
+ADD_REG_REG     = 0x13 # Add values from registers together into ACC
+PRINT_REGISTERS = 0x14 # Print values of all registers
+PUSH_LIT        = 0x15 # Push a literal value to the stack
+PUSH_REG        = 0x16 # Push a value from a register to the stack
+POP             = 0x17 # Pop a value from the stack
+CALL_LIT        = 0x18 # Make a call to a function whose pointer is a literal
+CALL_REG        = 0x19 # Make a call to a function whose pointer comes from a register
+RET             = 0x20 # Return back from a call instruction
 
 #Quick shortcut for printing
 def p(inp):
@@ -22,10 +30,14 @@ class computer:
     def __init__(self):
         # Create a memory region for the registers
         # a 32 bit field (4 bytes) for every register
-        self.registers = bytearray(6 * 4);
+        self.registers = bytearray(len(REGS) * 4);
         # Create the RAM
         self.memory = bytearray(512)
 
+
+        self.setregister(SP, 512-4)
+        self.setregister(FP, 512-4)
+        self.stackFrameSize = 0;
     # Function for putting stuff in the memory
     def upload_at_location(self, location, buffer):
         for x in range(0, len(buffer)):
@@ -33,8 +45,9 @@ class computer:
 
     # Quick way to debug what's going on
     def printallregisters(self):
-        for x in range(0, 6*4, 4):
-            print(REGS[int(x/4)] + ' 0x{:0<2x}{:0<2x}{:0<2x}{:0<2x}'.format(self.registers[x],self.registers[x+1],self.registers[x+2],self.registers[x+3]))
+        for x in range(0, len(REGS)*4, 4):
+            print(REGS[int(x/4)] + ' 0x{:0>2x}{:0>2x}{:0>2x}{:0>2x}'.format(self.registers[x],self.registers[x+1],self.registers[x+2],self.registers[x+3]))
+        print('\n')
 
     # Function for getting the full 32 bit value from a register
     def getregister(self, reg):
@@ -69,9 +82,66 @@ class computer:
         self.setregister(IP, reg+4)
         return value
     
+    # Function to get a 4 byte value from memory at a custom location
+    def fetch32at(self, location):
+        value = self.memory[location+3] + self.memory[location+2]*256 + self.memory[location+1]*256*256 + self.memory[location]*256*256*256
+        return value
+        
+    # Function to write to memory
+    def write32(self, location, value):
+        value = '{:0>8x}'.format(value)
+        values = [value[x:x+2] for x in range(0, len(value), 2)]
+        self.memory[location] = int(values[0],16)
+        self.memory[location+1] = int(values[1],16)
+        self.memory[location+2] = int(values[2],16)
+        self.memory[location+3] = int(values[3],16)
+    # Push a 32 bit value to the stack
+    def push(self, value):
+        spAddress = self.getregister(SP);
+        self.write32(spAddress, value);
+        self.setregister(SP,spAddress-4);
+        self.stackFrameSize += 4;
+
+    # Pop a 32 bit value from the stack
+    def pop(self):
+        nextSpAddress = self.getregister(SP) + 4;
+        self.setregister(SP, nextSpAddress);
+        self.stackFrameSize -= 4;
+        return self.fetch32at(nextSpAddress);
+
+    # Push the current state of the CPU to the stack
+    def pushState(self):
+        
+        self.push(self.getregister(R1))
+        self.push(self.getregister(R2))
+        self.push(self.getregister(R3))
+        self.push(self.getregister(R4))
+        self.push(self.getregister(IP))
+        self.push(self.stackFrameSize + 4)
+        
+        self.setregister(FP,self.getregister(SP))
+        self.stackFrameSize = 0;
+
+    # Get the current state back from the stack into the CPU
+    def popState(self):
+        framePointer = self.getregister(FP)
+        self.setregister(SP, framePointer)
+        self.stackFrameSize = self.pop()
+        tempStackFrame = self.stackFrameSize;
+        self.setregister(IP,self.pop())
+        self.setregister(R4,self.pop())
+        self.setregister(R3,self.pop())
+        self.setregister(R2,self.pop())
+        self.setregister(R1,self.pop())
+        numberOfArgs = self.pop()
+        for i in range(0, numberOfArgs):
+            self.pop()
+            
+        self.setregister(FP, framePointer + tempStackFrame)
+        
     # Execute the instruction passed to it
     def execute(self, instruction):
-        if(instruction == MOV_CONST_REG):
+        if(instruction == MOV_LIT_REG):
             value = self.fetch32()
             register = self.fetch()
             self.setregister(register, value)
@@ -86,33 +156,66 @@ class computer:
             self.setregister(ACC, value)
         if(instruction == PRINT_REGISTERS):
             self.printallregisters();
+        if(instruction == PUSH_LIT):
+            value = self.fetch32();
+            self.push(value);
+        if(instruction == PUSH_REG):
+            register = self.fetch();
+            value = self.getregister(register);
+            self.push(value);
+        if(instruction == POP):
+            register = self.fetch();
+            value = self.pop();
+            self.setregister(register, value);
+        if(instruction == CALL_LIT):
+            jumpAddress = self.fetch32()
+            self.pushState()
+            self.setregister(IP, jumpAddress);
+        if(instruction == CALL_REG):
+            jumpAddress = self.getregister(self.fetch())
+            self.pushState()
+            self.setregister(IP, jumpAddress);
+        if(instruction == RET):
+            self.popState();
             
     # Fetch and execute the next instruction
     def step(self):
         instruction = self.fetch();
         p(instruction)
-        self.execute(instruction);
+        return self.execute(instruction);
 
 
 
 my_computer = computer()
 # Create the buffer with all the "code" in it
-buffer = [MOV_CONST_REG, 0x12, 0x34, 0x56, 0x78, R1,
-          MOV_CONST_REG, 0x78, 0x56, 0x34, 0x12, R2,
-          ADD_REG_REG, R1, R2,
-          MOV_CONST_REG, 0x00, 0x00, 0x00, 0x00, R2,
-          MOV_REG_REG, ACC, R1,
-          PRINT_REGISTERS]
+buffer = [PUSH_LIT, 0x41,0x41,0x41,0x41,
+          PUSH_LIT, 0x42,0x42,0x42,0x42,
+          PUSH_LIT, 0x43,0x43,0x43,0x43,
+          
+          PUSH_LIT,0x00,0x00,0x00,0x03,
+          
+          CALL_LIT,0x00,0x00,0x01,0x00,
+          PRINT_REGISTERS
+          ]
 
-character_representation = ''.join([chr(x) for x in buffer])
-print("LENGTH OF PROGRAM: {0} CHARACTERS".format(len(character_representation)))
+
+buffer2 = [RET]
 
 #Upload code at position 0
 my_computer.upload_at_location(0,buffer)
+my_computer.upload_at_location(0x100,buffer2)
 
 # Step through the code one step at a time
 while 1:
     my_computer.step()
     i = input('>');
-    if(i=='a'):
-        my_computer.printallregisters()
+    if(len(i)>0):
+        if(i=='a'):
+            my_computer.printallregisters()
+        if(i[0]=='m'):
+            print(my_computer.memory)
+        if(i[0]=='s'):
+            print("STACK:")
+            for x in range(my_computer.getregister(SP),508,4):
+                print(hex(my_computer.fetch32at(x+4)))
+            print("\n")
